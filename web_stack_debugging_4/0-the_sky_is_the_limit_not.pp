798ee6876d96e762f24c1cac08d55ea1ffbe0c44
gichuri <m.ndungu@alustudent.com>
2022-12-13 20:02:05 +0300

    ulimit change

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,10 @@
 # change number of open files
 
 exec {'inc_file_limit':
-        command  => 'sed -i s/ULIMIT="-n 15"/ULIMIT="-n 4096"/g /etc/default/nginx',
-        provider => shell,
+    command  => 'sed -i s/ULIMIT="-n 15"/ULIMIT="-n 4096"/g /etc/default/nginx',
+    provider => shell,
 }
 exec {'service_nginx_restart':
-	command  => 'sudo service nginx restart', 
-	provider => shell,
+    command  => 'sudo service nginx restart',
+    provider => shell,
 }
