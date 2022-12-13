149afb79c10da4ba27cd9c1d1c46d4adcdaf3a88
gichuri <m.ndungu@alustudent.com>
2022-12-13 19:30:06 +0300

    ulimit change

---
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# change ulimit to 4096
+
+exec{'change_ulimit':
+	command => 'sed -i s/ULIMIT="-n 15"/ ULIMIT="-n 4096"/g /etc/default/nginx'
+	provider => shell,
+}
+# restart nginx
+service{'nginx':
+	restart => 'nginx',
+}
